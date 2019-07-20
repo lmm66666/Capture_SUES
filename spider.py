fefea1a07e1fc0d691ebbb965f6ba602f983e77f
lmm66666 <2586709056@qq.com>
2019-07-20 22:49:04 +0800

    continue tomorrow

--- spider.py
+++ spider.py
@@ -7,16 +7,19 @@ IDPath = '/Users/apple/Desktop/IDCode.png'
 data = {'loginForm.name': '021718120', 'loginForm.password': 'Laomengan39!', 'loginForm.captcha': ''}
 ID_src = 'http://jxxt.sues.edu.cn/eams/captcha/image.action'
 
-def getHTMLText(url):
+def getHTMLText():
     try:
-        r = requests.get(url, timeout=30)
+        r = requests.get(web, timeout=30)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         soup = bs4.BeautifulSoup(r.text, "html.parser")
     except:
         return "访问失败"
-    #print(soup.find('img', id='captcha')['src'])
-    #获取二维码图片
+    return soup
+
+
+def save_image():
+    # print(soup.find('img', id='captcha')['src'])
     if os.path.exists(IDPath):
         os.remove(IDPath)
         r_image = requests.get(ID_src)
@@ -24,10 +27,22 @@ def getHTMLText(url):
             f.write(r_image.content)
             f.close()
         print('Succceed!')
-    #解析二维码
 
 
-html = getHTMLText(web)
+def analysis_image():
+    code = input()
+    return code
+
+
+def login(code):
+    pass
+
+
+
+getHTMLText()
+save_image()
+code = analysis_image()
+login(code)
 
 
 
